3ef3796da5000822d4219170495976400a9bb7f6
anzwkk <anam89737@gmail.com>
2025-01-29 16:14:30 +0300

    the second largest element was found

--- HW9.1/Program.cs
+++ HW9.1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написати програму, що знаходить другий найбільший елемент масиву.
+int[] array = { 10, 20, 5, 8, 30, 25 };
+int max1 = array[0];
+int max2 = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max1)
+    {
+        max2 = max1;
+        max1 = array[i];
+    }
+    else if (array[i] > max2 && array[i] != max1)
+    {
+        max2 = array[i];
+    }
+}
+Console.WriteLine("Другий найбільший елемент: " + max2);
